b0551b6eca4d7b277e282a11e67de406624b69c3
Gerardo Chavez <info.gerardo.chavez@gmail.com>
2020-12-10 10:02:31 -0300

    Correcciones
    - Se mejoraron los nombres de las listas.
    - Se crea la identificación de elemento que mas se repitió

--- Practico9Ejercicio15/Ejercicio15.java
+++ Practico9Ejercicio15/Ejercicio15.java
@@ -27,9 +27,9 @@ public class Ejercicio15 {
         String BaseURL = "https://www.netflix.com/uy/";
         WebDriver driver = seleniumUtils.getUrlDriver(BaseURL);
 
-        List<WebElement> elemH1 = driver.findElements(By.tagName("h1"));
-        List<WebElement> elemH2 = driver.findElements(By.tagName("h2"));
-        List<WebElement> elemH3 = driver.findElements(By.tagName("h3"));
+        List<WebElement> listaDeH1 = driver.findElements(By.tagName("h1"));
+        List<WebElement> listaDeH2 = driver.findElements(By.tagName("h2"));
+        List<WebElement> listaDeH3 = driver.findElements(By.tagName("h3"));
 
         /*
         System.out.println(elemH1.size());
@@ -37,22 +37,22 @@ public class Ejercicio15 {
         System.out.println(elemH3.size());
 
         */
-        if (elemH1.size() > elemH2.size() && elemH1.size() > elemH3.size()) {
+        if (listaDeH1.size() > listaDeH2.size() && listaDeH1.size() > listaDeH3.size()) {
             System.out.println("*****Elementos H1*****");
-            for (WebElement element : elemH1) {
+            for (WebElement element : listaDeH1) {
                 System.out.println(element.getText());
 
             }
-        }else if (elemH2.size() > elemH1.size() && elemH2.size() > elemH3.size()) {
+        }else if (listaDeH2.size() > listaDeH1.size() && listaDeH2.size() > listaDeH3.size()) {
             System.out.println("*****Elementos H2*****");
-            for (WebElement element1 : elemH2) {
+            for (WebElement element1 : listaDeH2) {
                 System.out.println(element1.getText());
 
             }
 
         }else{
             System.out.println("*****Elementos H3*****");
-            for(WebElement element2 : elemH3){
+            for(WebElement element2 : listaDeH3){
                 System.out.println((element2.getText()));
 
             }
@@ -60,43 +60,62 @@ public class Ejercicio15 {
         }
         driver.navigate().refresh();
 
-        List<WebElement> button = driver.findElements(By.tagName("button"));
-        for (WebElement elementb : button){
+        List<WebElement> listaDeButton = driver.findElements(By.tagName("button"));
+        for (WebElement elementb : listaDeButton){
             System.out.println(elementb.getText());
 
         }
 
         driver.manage().window().maximize();
 
-        List<WebElement> div = driver.findElements(By.tagName("div"));
-        System.out.println("La cantidad de elementos div es: " + div.size());
+        List<WebElement> listaDeDivs = driver.findElements(By.tagName("div"));
+        System.out.println("La cantidad de elementos div es: " + listaDeDivs.size());
 
         String titulo = driver.getTitle();
         System.out.println("El titulo de la pagina es: " + titulo);
 
-        List<WebElement> input = driver.findElements(By.tagName("input"));
-        System.out.println("La cantidad de elementos input es: " + input.size());
+        List<WebElement> listaDeInput = driver.findElements(By.tagName("input"));
+        System.out.println("La cantidad de elementos input es: " + listaDeInput.size());
 
-        List<WebElement> link = driver.findElements(By.tagName("link"));
-        System.out.println("La cantidad de elementos link es: " + link.size());
+        List<WebElement> listaDeLinks = driver.findElements(By.tagName("link"));
+        System.out.println("La cantidad de elementos link es: " + listaDeLinks.size());
 
         List<Integer> maximo = new ArrayList<Integer>();
+        List<List> listaDeListas = new ArrayList<>();
 
-        maximo.add(elemH1.size());
-        maximo.add(elemH2.size());
-        maximo.add(elemH3.size());
-        maximo.add(button.size());
-        maximo.add(div.size());
-        maximo.add(input.size());
-        maximo.add(link.size());
+
+
+        maximo.add(listaDeH1.size());
+        maximo.add(listaDeH2.size());
+        maximo.add(listaDeH3.size());
+        maximo.add(listaDeButton.size());
+        maximo.add(listaDeDivs.size());
+        maximo.add(listaDeInput.size());
+        maximo.add(listaDeLinks.size());
+
+        List<String> nombreDeLista = new ArrayList<String>();
+
+        nombreDeLista.add("Lista de H1s");
+        nombreDeLista.add("Lista de H2s");
+        nombreDeLista.add("Lista de H3s");
+        nombreDeLista.add("Lista de Buttons");
+        nombreDeLista.add("Lista de Divs");
+        nombreDeLista.add("Lista de Input");
+        nombreDeLista.add("Lista de Links");
+
+
         int max = 0;
+        String masCantidad = "";
+
         for(int i = 0; i < maximo.size(); i++){
             if(maximo.get(i)> max){
                 max = maximo.get(i);
+                masCantidad = nombreDeLista.get(i);
 
             }
         }
-        System.out.println("El elemento que mas se repite es " + max);
+
+        System.out.println("El elemento que mas se repite es " + masCantidad + " con " +  max + " elementos");
         // verificacion System.out.println(maximo);
 
         driver.quit();
